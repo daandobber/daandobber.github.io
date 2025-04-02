dc91d52fb55356b9adc910b0c467fcedb05ab437
daandobber <79364702+daandobber@users.noreply.github.com>
2025-04-02 11:49:26 +0200

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,89 +1,96 @@
 import type { Config } from "tailwindcss";
 
 export default {
-	plugins: [require("@tailwindcss/typography")],
-	theme: {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"],
+
+    safelist: [
+		{
+			pattern: 'noUi-.*',
+		},
+	],
+
+    plugins: [require("@tailwindcss/typography")],
+    theme: {
 		extend: {
 			typography: () => ({
 				DEFAULT: {
 					css: {
 						a: {
-							textUnderlineOffset: "2px",
-							"&:hover": {
-								"@media (hover: hover)": {
-									textDecorationColor: "var(--color-link)",
-									textDecorationThickness: "2px",
-								},
-							},
+							textUnderlineOffset: '2px',
+							'&:hover': {
+								'@media (hover: hover)': {
+									textDecorationColor: 'var(--color-link)',
+									textDecorationThickness: '2px'
+								}
+							}
 						},
 						blockquote: {
-							borderLeftWidth: "0",
+							borderLeftWidth: '0'
 						},
 						code: {
-							border: "1px dotted #666",
-							borderRadius: "2px",
+							border: '1px dotted #666',
+							borderRadius: '2px'
 						},
 						kbd: {
-							"&:where([data-theme='dark'], [data-theme='dark'] *)": {
-								background: "var(--color-global-text)",
-							},
+							'&:where([data-theme="dark"], [data-theme="dark"] *)': {
+								background: 'var(--color-global-text)'
+							}
 						},
 						hr: {
-							borderTopStyle: "dashed",
+							borderTopStyle: 'dashed'
 						},
 						strong: {
-							fontWeight: "700",
+							fontWeight: '700'
 						},
 						sup: {
-							marginInlineStart: "calc(var(--spacing) * 0.5)",
+							marginInlineStart: 'calc(var(--spacing) * 0.5)',
 							a: {
-								"&:after": {
-									content: "']'",
+								'&:after': {
+									content: "']'"
 								},
-								"&:before": {
-									content: "'['",
+								'&:before': {
+									content: "'['"
 								},
-								"&:hover": {
-									"@media (hover: hover)": {
-										color: "var(--color-link)",
-									},
-								},
-							},
+								'&:hover': {
+									'@media (hover: hover)': {
+										color: 'var(--color-link)'
+									}
+								}
+							}
 						},
-						/* Table */
-						"tbody tr": {
-							borderBottomWidth: "none",
+						'tbody tr': {
+							borderBottomWidth: 'none'
 						},
 						tfoot: {
-							borderTop: "1px dashed #666",
+							borderTop: '1px dashed #666'
 						},
 						thead: {
-							borderBottomWidth: "none",
+							borderBottomWidth: 'none'
 						},
-						"thead th": {
-							borderBottom: "1px dashed #666",
-							fontWeight: "700",
+						'thead th': {
+							borderBottom: '1px dashed #666',
+							fontWeight: '700'
 						},
 						'th[align="center"], td[align="center"]': {
-							"text-align": "center",
+							'text-align': 'center'
 						},
 						'th[align="right"], td[align="right"]': {
-							"text-align": "right",
+							'text-align': 'right'
 						},
 						'th[align="left"], td[align="left"]': {
-							"text-align": "left",
-						},
-					},
+							'text-align': 'left'
+						}
+					}
 				},
 				sm: {
 					css: {
 						code: {
-							fontSize: "var(--text-sm)",
-							fontWeight: "400",
-						},
-					},
-				},
-			}),
-		},
-	},
+							fontSize: 'var(--text-sm)',
+							fontWeight: '400'
+						}
+					}
+				}
+			})
+		}
+	}
 } satisfies Config;
